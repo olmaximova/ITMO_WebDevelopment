783ceea7f39f907902622df8d0702bc209b7b3e1
olmaximova <ol.maximova1309@gmail.com>
2025-09-07 21:51:10 +0300

    added cart and implemented the cart opening functio

---
+++ lab1/main.js
@@ -0,0 +1,7 @@
+let cartIcon = document.querySelector('.btn__cart');
+let body = document.querySelector('body');
+let closeButton = document.querySelector('.close_button')
+
+cartIcon.addEventListener('click', () => {
+    body.classList.toggle('show__cart')
+})
